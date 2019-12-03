ba7c0a5a49aa7a2cb4afdddd24f82efb361bda99
Carlos A. Moncada <cmoncada@users.noreply.github.com>
2019-12-03 14:13:18 -0500

    initial

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function About() {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>this is about section</p>
+    </Fragment>
+  );
+}
+
+export default About;
